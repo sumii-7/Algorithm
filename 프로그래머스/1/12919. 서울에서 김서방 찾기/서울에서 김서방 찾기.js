5f85f1c4f142973eb1d0dcf4e3ede694d0bbec7d
sumii-7 <155044540+sumii-7@users.noreply.github.com>
2024-10-17 16:46:11 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.04 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
@@ -1,7 +1,3 @@
 function solution(seoul) {
-    var answer
-    for(let i = 0; i < seoul.length; i++){
-        answer = seoul.indexOf("Kim")
-    }
-    return `김서방은 ${answer}에 있다`
+    return `김서방은 ${seoul.indexOf("Kim")}에 있다`
 }
