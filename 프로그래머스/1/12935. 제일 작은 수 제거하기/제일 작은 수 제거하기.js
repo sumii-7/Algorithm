88f4aaeae9193446dc66a3615092eb97cf8c58ce
sumii-7 <155044540+sumii-7@users.noreply.github.com>
2024-10-22 20:34:11 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.53 ms, Memory: 42.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
@@ -0,0 +1,10 @@
+function solution(arr) {
+    if(arr.length === 1){
+        return [-1]
+    }
+    
+    const min = arr.indexOf(Math.min(...arr))
+    arr.splice(min, 1)
+    
+    return arr;
+}
