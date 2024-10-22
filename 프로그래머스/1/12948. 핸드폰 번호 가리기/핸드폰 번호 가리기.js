147f236ab78deb6d5153192d6163fbff4c53867a
sumii-7 <155044540+sumii-7@users.noreply.github.com>
2024-10-22 21:19:36 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -0,0 +1,7 @@
+function solution(phone_number) {
+    var answer = '';
+    for(let i = 0; i < phone_number.length - 4; i++){
+        answer += "*";
+    }
+    return answer += phone_number.slice(-4);
+}
